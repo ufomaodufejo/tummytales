20bf3a4571ebc0b9fee7621fa5156c984169ada8
Ufoma Ibukunoluwa Odufejo <ufomaodufejo@gmail.com>
2022-11-04 12:51:09 +0100

    sixth commit

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -5,21 +5,22 @@ function Orders(props) {
   return (
     <div className="categories">
       <div className="gallery-category">
-        <div className="gallerymenu">
+
+        <div className="gallery">
           <img src={props.img1} alt="smaachops" />
           <Link to="login">
             <button className="btn1">Order Now</button>
           </Link>
         </div>
 
-        <div className="gallerymenu">
+        <div className="gallery">
           <img src={props.img2} alt="smaachops" />
           <Link to="login">
             <button className="btn1">Order Now</button>
           </Link>
         </div>
 
-        <div className="gallerymenu">
+        <div className="gallery">
           <img src={props.img3} alt="smaachops" />
           <Link to="login">
             <button className="btn1">Order Now</button>
